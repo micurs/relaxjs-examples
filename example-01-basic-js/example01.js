621e5d86b55475b955c5c37434c33eba7103990d
Michele Ursino <michele@micurs.com>
2015-06-27 11:54:52 -0700

    First relaxjs example in pure javascript (includes VS Code launch.json configuration file).

---
+++ example-01-basic-js/example01.js
@@ -0,0 +1,67 @@
+// relaxjs example #01 - Simple resources
+
+var r = require('relaxjs');
+
+// Create the application by assembling the resources
+var site = r.site('Example #01');
+
+site.add(  {
+  name: 'Page1',
+  view: 'helloworld',
+  data: { message: "Hello World!" }
+});
+
+site.add( {
+  name: 'Page2',
+  view: 'helloworld',
+  data: { message: "Asta la vista!" }
+});
+
+site.add( {
+  name: 'ContainerPage',
+  view: 'helloworld',
+  data: { message: "This is a resource with child resources" },
+  resources: [
+    {
+      name: 'Child1',
+      view: 'helloworld',
+      data: { message: "Hello! This is child #1 !" }
+    },
+    {
+      name: 'Child2',
+      view: 'helloworld',
+      data: { message: "Hello! This is child #2 !" }
+    }
+  ]
+});
+
+function getCurrentDate() {
+  var date = new Date();
+  return ''+date.getHours()+':'+date.getMinutes()+'.'+date.getSeconds()+' UTC';
+}
+
+var staticResource = {
+  name: 'static',
+  view : 'helloworld',
+  data : { message: 'Hello! This resource was created on '+ getCurrentDate() }
+};
+
+var dynamicResource = {
+  name: 'dynamic',
+  view : 'helloworld',
+  onGet : function( query, respond ) {
+    var date = new Date();
+    this.data = { message: 'Hello Dynamic World! It is now '+ getCurrentDate()  };
+    respond.ok();
+  }
+};
+
+
+site.add( staticResource );
+site.add( dynamicResource );
+
+// Create the application server for the site
+var appSrv = site.serve();
+
+// Listen
+appSrv.listen(3030);
